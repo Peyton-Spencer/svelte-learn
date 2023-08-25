a1fd67ea069027fec63eca9cce5d588481aafd90
peyton-spencer <pas1234567@gmail.com>
2023-08-24 22:06:14 -0600

    fix username client validation. todo update instead of create

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import AuthCheck from "$lib/components/AuthCheck.svelte";
   import { db, user } from "$lib/firebase";
-  import { doc, getDoc, writeBatch } from "firebase/firestore";
+  import { collection, doc, getDoc, writeBatch } from "firebase/firestore";
 
   let username = "";
+  let currentUsername = "";
+  let doIOwnUsername = false;
   let loading = false;
   let isAvailable = false;
 
@@ -22,17 +24,53 @@
         return;
       }
       const ref = doc(db, "usernames", username);
-      const exists = await getDoc(ref).then((doc) => doc.exists());
-
+      const myDoc = await getDoc(ref);
+      const exists = myDoc.exists();
+      if (exists) {
+        console.log("Username already taken");
+        // alert(
+        //   "Username already taken by: " +
+        //     myDoc.get("uid") +
+        //     "\n your username: " +
+        //     $user!.uid
+        // );
+        if (myDoc.get("uid") === $user!.uid) {
+          currentUsername = username;
+          doIOwnUsername = true;
+        } else {
+          doIOwnUsername = false;
+        }
+      }
       isAvailable = !exists;
       loading = false;
     }, 500);
   }
-  async function confirm() {}
+  async function confirm() {
+    if ($user == null) {
+      console.log("No user");
+      return;
+    }
+    console.log("Confirming username: ", username);
+    const batch = writeBatch(db);
+    batch.set(doc(db, "usernames", username), { uid: $user!.uid });
+    batch.set(doc(db, "users", $user!.uid), {
+      username,
+      photoURL: $user?.photoURL,
+      published: true,
+      bio: "I am a friend of the trees",
+      links: [{ title: "Test Link", url: "https://kung.foo", icon: null }],
+    });
+    await batch.commit();
+    isAvailable = false;
+    doIOwnUsername = true;
+    currentUsername = username;
+  }
 </script>
 
 <AuthCheck>
   <h2>Select your username</h2>
+  <h3>Current User: {$user?.uid}</h3>
+  <h3>Current Username: {currentUsername}</h3>
   <form class="w-2/5" on:submit|preventDefault={confirm}>
     <input
       type="text"
@@ -41,19 +79,41 @@
       bind:value={username}
       on:input={checkAvailability}
     />
+
     {#if !username}
       <p>Please pick a username</p>
     {:else if loading}
       <p>Loading...</p>
-    {:else if isAvailable === true}
-      <p class="text-green-500">Yes, {username} is available!</p>
-    {:else if isAvailable === false}
+    {:else if isAvailable}
+      <p class="text-green-500">
+        {username} is available!
+      </p>
+      <p>
+        <small> 🎄💯🔥 LESS GOOOO 🎄</small>
+      </p>
+    {:else if !doIOwnUsername}
       <p class="text-red-500">
         Sorry, {username} is already taken. Please choose another 😭
       </p>
+      <p>
+        {#if currentUsername}
+          <small>
+            You can still use @{currentUsername} if you like.
+          </small>
+        {/if}
+      </p>
+    {:else}
+      <p class="text-green-500">You already own this username! 🎉</p>
     {/if}
-    {#if username && isAvailable}
-      <button class="btn btn-success">Confirm username @{username} </button>
+
+    {#if username}
+      {#if !doIOwnUsername && isAvailable}
+        <button class="btn btn-success">Confirm username: @{username} </button>
+      {:else if isAvailable}
+        <button class="btn btn-success"
+          >Update username to: @{username}
+        </button>
+      {/if}
     {/if}
   </form>
 </AuthCheck>
